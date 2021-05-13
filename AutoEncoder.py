805dc68a0fc43348b5b820ea79861b991a9fb63a
rli <rli@pnimedia.com>
2021-05-13 10:39:31 -0700

    back

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -10,17 +10,18 @@ import tensorflow as tf
 from keras.layers import Input, Dropout, Dense, LSTM, TimeDistributed, RepeatVector
 from keras.models import Model
 from keras import regularizers
+from keras.utils import plot_model
 
 # define the autoencoder network model
 def autoencoder_model(X):
-    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    inputs = Input(shape=(X.shape[0], X.shape[1]))
     L1 = LSTM(16, activation='relu', return_sequences=True,
               kernel_regularizer=regularizers.l2(0.00))(inputs)
     L2 = LSTM(4, activation='relu', return_sequences=False)(L1)
-    L3 = RepeatVector(X.shape[1])(L2)
+    L3 = RepeatVector(X.shape[0])(L2)
     L4 = LSTM(4, activation='relu', return_sequences=True)(L3)
     L5 = LSTM(16, activation='relu', return_sequences=True)(L4)
-    output = TimeDistributed(Dense(X.shape[2]))(L5)
+    output = TimeDistributed(Dense(X.shape[1]))(L5)
     model = Model(inputs=inputs, outputs=output)
     return model
 
@@ -44,5 +45,10 @@ arr = dataset[["erro_rate", "avg_duration"]].to_numpy()
 scaler = MinMaxScaler()
 training = scaler.fit_transform(arr)
 # print(training)
+
+nn = autoencoder_model(training)
+nn.compile()
+nn.summary()
+plot_model(nn, show_shapes=True, to_file="mode_architecture.png")
 
 
