35f572c8e0e09095b8cbf32dfc6d1b8e6888b2cc
rli <rli@pnimedia.com>
2021-05-14 13:55:48 -0700

    back

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import math
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -34,7 +35,6 @@ def plot_train_history(history, title):
 def plot_loss(ds_result, column_to_display):
     plt.figure(figsize=(16,9), dpi=80)
     plt.title('Loss Distribution', fontsize=16)
-    # sns.distplot(scored[column_to_display], bins = 20, kde = True, color = "blue")
     sns.histplot(ds_result[column_to_display], bins = 20, kde = True, color = "blue")
     plt.xlim([0.0,.5])
     plt.show()
@@ -66,6 +66,7 @@ def sequential_model(inputshape):
         Dense(units=inputshape[1])))
     return model
 
+# define get data from cvs file
 def data_from_csv(data_folder, block_size):
     parts = []
     custom_date_parser = lambda x: datetime.strptime(x, "%m/%d/%Y, %I:%M:%S.%f %p")
@@ -83,7 +84,15 @@ def data_from_csv(data_folder, block_size):
     train_data = temp.reshape(originalShape[0] // block_size, block_size, originalShape[1])
     return (train_data, dataset)
 
+# define build loss data set
+def build_loss(pred, actual, col_loss, dataset_index):
+    pred1 = pred.reshape(pred.shape[0] * pred.shape[1], pred.shape[2])
+    actual1 = actual.reshape(actual.shape[0] * actual.shape[1], actual.shape[2])
+    ds_loss =  pd.DataFrame()
+    ds_loss[col_loss] = np.mean(np.abs(pred1 - actual1), axis=1)
+    return ds_loss
 
+loss_column_name = "loss_mae"
 epochs = 1250
 batch_size = 36
 sns.set(color_codes=True, rc={'figure.figsize':(11, 4)})
@@ -93,10 +102,21 @@ sns.set(color_codes=True, rc={'figure.figsize':(11, 4)})
 nn = sequential_model(training.shape[-2:])
 nn.compile(loss="mae", optimizer="adam")
 # plot_model(nn, show_shapes=True, to_file="mode_architecture.png")
+
 history = nn.fit(training, training, batch_size=batch_size, epochs=epochs, validation_split=0.05, shuffle=True, verbose=False)
 plot_train_history(history, "train")
 
-# nn.save("test_model")
+test = training[0:5,:,:]
+pred = nn.predict(test)
+ds1 = build_loss(pred, test, loss_column_name, ds_train.index)
+plot_loss(ds1, loss_column_name)
+loss_column = ds1[loss_column_name]
+mean = loss_column .mean()
+sigma = loss_column .std()
+threshold = mean + 1.96*sigma/math.sqrt(len(loss_column)) # 95%
+print(f"Threshold: {threshold}")
+
+nn.save("test_model")
 
 
 
